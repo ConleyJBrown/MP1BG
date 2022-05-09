fcc4f8eeb852e372e6f8b506494e62a8e451fa00
Conley Brown <conleybrown@Conleys-MacBook-Pro.local>
2022-05-09 08:13:39 -0400

    linked js to html and created array of country objects

---
+++ script.js
@@ -0,0 +1,54 @@
+countryNames = [
+    "Afghanistan", "Albania", "Algeria", "Andorra",
+    "Angola", "Antigua and Barbuda", "Argentina", "Armenia",
+    "Australia", "Austria","Azerbaijan","Bahamas","Bahrain",
+    "Bangladesh", "Barbados","Belarus", "Belgium", "Belize",
+    "Benin","Bermuda","Bhutan","Bolivia", "Bosnia and Herzegovina",
+    "Botswana", "Brazil", "Brunei", "Bulgaria","Burkina Faso",
+    "Burundi", "Cambodia", "Cameroon","Canada", "Cape Verde", 
+    "Cayman Islands", "Central African Republic", "Chad", "Chile",
+    "China", "Colombia", "Comoros", "Cook Islands", "Costa Rica",
+    "Croatia", "Cuba", "Cyprus", "Czech Republic", "Democractic Republic of the Congo",
+    "Denmark", "Djibouti", "Dominica", "Dominican Republic",
+    "East Timor", "Ecuador", "Egypt", "El Salvador", "England",
+    "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji",
+    "Finland", "France", "Gabon", "Georgia", "Germany", "Ghana",
+    "Greece","Greenland","Grenada","Guam","Guatemala","Guinea",
+    "Guinea-Bissau","Guyana","Haiti","Honduras","Hong Kong", "Hungary",
+    "Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy",
+    "Ivory Coast","Jamaica","Japan","Jordan","Kazakhstan","Kenya",
+    "Kiribati","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho",
+    "Liberia","Libya","Liechtenstein","Lithuania","Luxembourg",
+    "Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali",
+    "Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia",
+    "Montenegro","Morocco","Mozambique","Myanmar","Namibia","Nauru",
+    "Nepal", "Netherlands","New Caledonia", "New Zealand","Nicaragua",
+    "Niger", "Nigeria","North Korea","Norway","Oman","Pakistan","Palau",
+    "Palestine", "Panama", "Papau New Guinea", "Paraguay", "Peru", "Philippines",
+    "Poland", "Portugal", "Puerto Rico", "Qatar","Republic of the Congo",
+    "Romania", "Russia","Rwanda","Saint Kitts and Nevis","Saint Lucia",
+    "Saint Vincent and the Grenadines","Samoa","San Marino","Sao Tome and Principe",
+    "Saudi Arabia","Scotland","Senegal","Serbia","Seychelles", "Sierra Leone",
+    "Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa",
+    "South Korea", "South Sudan","Spain","Sri Lanka","Sudan","Suriname",
+    "Swaziland", "Sweden","Swittzerland","Syria","Taiwan","Tajikistan",
+    "Tanzania","Thailand","The Gambia","Togo","Tonga","Trinidad and Tobago",
+    "Tunisia","Turkey","Turkmenistan","Tuvalu","Uganda","Ukraine",
+    "UAE","United Kingdom","United States","Uruguay","Uzbekistan","Vanuatu",
+    "Venezuela","Vietnam","Wales","Yemen","Zambia","Zimbabwe"
+]
+
+countries = []
+
+class Country{
+    constructor(name, imagesource)
+    {
+        this.name = name;
+        this.imagesource = imagesource;
+    }
+}
+
+function addNewCountry(name){
+    let newCountry = new Country(name, "name.jpeg");
+    countries.push(newCountry);
+}
