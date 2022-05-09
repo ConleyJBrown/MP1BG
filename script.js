7e9760e1994bbbe76a55ea88f42b68bbc562e905
Conley Brown <conleybrown@Conleys-MacBook-Pro.local>
2022-05-09 15:25:21 -0400

    make the next question load upon answering the previous question

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ countryNames = [
     "Montenegro","Morocco","Mozambique","Myanmar","Namibia","Nauru",
     "Nepal", "Netherlands","New Caledonia", "New Zealand","Nicaragua",
     "Niger", "Nigeria","North Korea","Norway","Oman","Pakistan","Palau",
-    "Palestine", "Panama", "Papau New Guinea", "Paraguay", "Peru", "Philippines",
+    "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines",
     "Poland", "Portugal", "Puerto Rico", "Qatar","Republic of the Congo",
     "Romania", "Russia","Rwanda","Saint Kitts and Nevis","Saint Lucia",
     "Saint Vincent and the Grenadines","Samoa","San Marino","Sao Tome and Principe",
@@ -34,7 +34,7 @@ countryNames = [
     "Swaziland", "Sweden","Swittzerland","Syria","Taiwan","Tajikistan",
     "Tanzania","Thailand","The Gambia","Togo","Tonga","Trinidad and Tobago",
     "Tunisia","Turkey","Turkmenistan","Tuvalu","Uganda","Ukraine",
-    "UAE","United Kingdom","United States","Uruguay","Uzbekistan","Vanuatu",
+    "United Arab Emirates","United Kingdom","United States","Uruguay","Uzbekistan","Vanuatu",
     "Venezuela","Vietnam","Wales","Yemen","Zambia","Zimbabwe"
 ]
 
@@ -61,47 +61,59 @@ let result = document.getElementById("whetherCorrect");
 let scoreTable = document.getElementById("yourScore");
 let score = 0;
 let attempts = 0;
-let quit = false;
+let randomCountry;
+window.onload = nextQuestion();
 
-let randomCountry = countries[Math.floor(Math.random()*countries.length)];
-
-console.log(randomCountry);
-
-
-let image = document.getElementsByClassName("flagImage")[0];
-image.src = randomCountry.imageSource;
-
-let correctAnswer = Math.floor(Math.random()*4+1);
-console.log(correctAnswer);
-
-let possibleAnswers = countryNames;
-for( let i = 0; i < possibleAnswers.length; i++){ 
-    
-    if ( possibleAnswers[i] === randomCountry.name) { 
-
-        possibleAnswers.splice(i, 1); 
-    }
-
-}
-
-
-for(let i = 1; i <=4;  i++)
+function nextQuestion()
 {
-    let button = document.getElementById("button"+i);
-    if(i === correctAnswer)
-    {
-        button.textContent = randomCountry.name;
-        button.addEventListener("click", handleCorrectAnswer);
-    }
-    else
-    {
-       let wrongAnswerIndex = Math.floor(Math.random()*countries.length);
-        button.textContent = possibleAnswers[wrongAnswerIndex];
-        possibleAnswers.splice(wrongAnswerIndex,1);
-        button.addEventListener("click", handleIncorrectAnswer);
+    randomCountry = countries[Math.floor(Math.random()*countries.length)];
+    console.log(randomCountry);
+    let image = document.getElementsByClassName("flagImage")[0];
+    image.src = randomCountry.imageSource;
+
+    let correctAnswer = Math.floor(Math.random()*4+1);
+    console.log(correctAnswer);
+
+    let possibleAnswers = countryNames;
+    for( let i = 0; i < possibleAnswers.length; i++){ 
+    
+        if ( possibleAnswers[i] === randomCountry.name) { 
+
+            console.log(possibleAnswers.splice(i, 1)+" was removed from possible incorrect answers"); 
+
+        }
 
     }
+
+    for(let i = 1; i <=4;  i++)
+    {
+        let button = document.getElementById("button"+i);
+        if(i === correctAnswer)
+        {
+            button.textContent = randomCountry.name;
+            console.log(randomCountry.name+ " was placed in button " + i + "and is the correct answer");
+            button.addEventListener("click", handleCorrectAnswer);
+            console.log("button" + i + " is listening for correct response");
+        }
+        else
+        {
+            let wrongAnswerIndex = Math.floor(Math.random()*countries.length);
+            button.textContent = possibleAnswers[wrongAnswerIndex];
+            console.log(possibleAnswers.splice(wrongAnswerIndex,1) + " has been removed and placed on button " + i) ;
+            button.addEventListener("click", handleIncorrectAnswer);
+            console.log("button " + i + " is listening for incorrect response");
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
 
 function handleCorrectAnswer()
 {
@@ -110,6 +122,7 @@ function handleCorrectAnswer()
     score++;
     attempts++;
     scoreTable.textContent = "Your Score: " + score + "/" + attempts;
+    setTimeout(nextQuestion, 1000);
 }
 
 function handleIncorrectAnswer()
@@ -118,5 +131,5 @@ function handleIncorrectAnswer()
     result.textContent = "Incorrect. That was the flag of " + randomCountry.name + ".";
     attempts++;
     scoreTable.textContent = "Your Score: " + score + "/" + attempts;
-    
+    setTimeout(nextQuestion, 1000);
 }
